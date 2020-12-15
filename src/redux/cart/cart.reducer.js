4e141451eca43be6ed5ac8e65fae30bfe7243aac
Dhrunit <jimmyprajapati99@gmail.com>
2020-12-15 16:36:53 +0530

    Triggered actions using redux to add to cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,6 @@
-import { CartActionTypes } from './cart.types';
+import { CartActionTypes } from './cart.types'
 
-const INITIAL_STATE = { hidden: true };
+const INITIAL_STATE = { hidden: true, cartItems: [] }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
@@ -8,11 +8,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 			return {
 				...state,
 				hidden: !state.hidden,
-			};
-
+			}
+		case CartActionTypes.ADD_ITEM:
+			return { ...state, cartItems: [...state.cartItems, action.payload] }
 		default:
-			return state;
+			return state
 	}
-};
+}
 
-export default cartReducer;
+export default cartReducer
