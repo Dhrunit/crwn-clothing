37b48a0e48787f30bdbb6337356006d68a8328e6
Dhrunit <jimmyprajapati99@gmail.com>
2020-12-15 14:50:08 +0530

    Added logic to conditionally render cart dropdown using redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 
-import rootReducer from './root-reducer';
+import rootReducer from './root-reducer'
 
-const middlewares = [logger];
+const middlewares = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-export default store;
+export default store
